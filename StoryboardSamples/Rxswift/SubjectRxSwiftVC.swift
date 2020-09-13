65fea50a9dd803b0aef755a92d058654a0bb9d7b
Heewook Shin <saroby@naver.com>
2020-09-13 23:18:39 +0900

    Subject에 AsyncSubject 추가.

--- StoryboardSamples/Rxswift/SubjectRxSwiftVC.swift
+++ StoryboardSamples/Rxswift/SubjectRxSwiftVC.swift
@@ -11,8 +11,9 @@
 //      Relay는 RxCocoa에 들어있다.
 // 종류:
 // PublishSubject: 초기값이 없음. 구독자는 구독 후에 방출된 데이터만 전달받을 수 있다.
-// BehaviorSubject: 초기값이 존재. 구독자는 구독 직후 현재 subject가 저장하고 있는 값을 전달받는다. 그 이후는 PublishSubject와 같다. (Variable가 deprecate되고 이 subject로 교체됨)
+// BehaviorSubject: 초기값이 존재. 구독자는 구독 직후 현재 subject가 저장하고 있는 값을 전달 받는다. 그 이후는 PublishSubject와 같다. (Variable가 deprecate되고 이 subject로 교체됨)
 // ReplaySubject: 초기값 없음. 구독을 시작하면, 구독자는 이전에 subject가 발행한 모든 데이터들 중에 subject의 buffer만큼  전달받는다. 이후는 PublishSubject와 같다.
+// AsyncSubject: 초기값 없음. 구독자는 subject가 complete될 때, 저장된 마지막 데이터를 전달 받는다. 즉, subject는 complete 때 이외에는 데이터를 발행하지 않는다.
 
 import UIKit
 import RxSwift
@@ -36,6 +37,11 @@ class SubjectRxSwiftVC: UIViewController {
         
         print("ReplaySubject():")
         testReplaySubject()
+        
+        print()
+        
+        print("AsyncSubject():")
+        testAsyncSubject()
     }
     
     private func testPublishSubject() {
@@ -130,6 +136,43 @@ class SubjectRxSwiftVC: UIViewController {
 
         subject.onNext(3)
 
+        subscriptionOne.dispose()
+
+        subject.onNext(4)
+        subject.onCompleted()
+        subject.onNext(5)
+
+        subscriptionTwo.dispose()
+
+        let disposeBag = DisposeBag()
+
+        subject
+            .subscribe {
+                print("3)", $0.element ?? $0)
+            }
+            .disposed(by: disposeBag)
+
+        subject.onNext(9999)
+    }
+    
+    private func testAsyncSubject() {
+        let subject = AsyncSubject<Int>()
+
+        let subscriptionOne = subject
+            .subscribe(onNext: { num in
+                print("1)  \(num)")
+            })
+        subject.on(.next(1))
+        subject.onNext(2)
+
+        let subscriptionTwo = subject
+            .subscribe { event in
+                print("2)", event.element ?? event)
+            }
+
+        subject.onNext(3)
+
+        // 당연한 이야기지만, dispose된 구독자1은 이벤트를 받지 못한다.
         subscriptionOne.dispose()
 
         subject.onNext(4)
